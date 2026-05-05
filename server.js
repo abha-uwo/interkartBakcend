7d03479b60f3203d6c6f64b52dd05be619f81f05
aditi-050804 <aditi@uwo24.com>
2026-05-05 12:12:57 +0530

    fixed cors and allowed all origins

--- server.js
+++ server.js
@@ -40,7 +40,11 @@ async function syncKnowledgeBase() {
 }
 syncKnowledgeBase();
 
-app.use(cors());
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
